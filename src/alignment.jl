93cc26df3f96df694b30590e1365deb84692616f
Neel Smith <dnsmith.neel@gmail.com>
2024-03-01 10:08:20 -0500

    Fix bug in tracking array index

--- src/alignment.jl
+++ src/alignment.jl
@@ -9,21 +9,19 @@ function alignpair(s1,s2)
 	s1seen = 1
 	s2seen = 1
 	for (i, item) in enumerate(slots)
-		
-		if slots[i] == s1[s1seen]
+		if s1seen <= length(s1) && slots[i] == s1[s1seen]
 			push!(s1align, s1[s1seen])
 			s1seen = s1seen + 1
 		else
 			push!(s1align, nothing)
 		end
-		
-		if slots[i] == s2[s2seen]
+
+		if s2seen <= length(s2) && slots[i] == s2[s2seen]
 			push!(s2align, s2[s2seen])
 			s2seen = s2seen + 1
 		else
 			push!(s2align, nothing)
 		end
-		
 	end
 	s1align, s2align
 end
--- test/test_align.jl
+++ test/test_align.jl
@@ -1,12 +1,14 @@
 @testset "Test alignment" begin
-
-    ## Need to test array size before indexing!
-    @test_broken align("abcf", "bce") == (
+    
+    @test align("abcf", "bce") == [
         ['a','b', 'c', nothing, 'f'],
         [nothing, 'b', 'c', 'e', nothing]
-    )
+    ]
 
-    # align("ab", "bc")
+    @test align("ab", "bc") == [
+        ['a', 'b', nothing],
+        [nothing, 'b', 'c']
+    ]
 
     
 end
